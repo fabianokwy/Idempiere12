b0f708b5a66bc270dbf18a4de2e954bf66599fc9
Fabiano Gomes <fabianokwy@gmail.com>
2025-11-09 12:22:05 -0400

    Limpeza e adição do .gitignore otimizado (PRODEXA Edition)

--- org.idempierelbr.core/migration/i2.0/oracle/201603311324-Sped-000-removeviews.sql
+++
@@ -1,9 +0,0 @@
--- Sped EFD
--- This script was generated for a postgresql DB. It should be tested in a oracle DB.
--- DROP VIEW LBR_FactFiscal;
--- DROP VIEW LBR_FactFiscal_NotaFiscalTax;
--- DROP VIEW LBR_FactFiscal_NFLineTax;
--- DROP VIEW LBR_FactFiscal_InvoiceTax;
--- DROP VIEW LBR_FactFiscal_InvoiceLineTax;
-
-SELECT lbr_register_migration_script('201603311324-Sped-000-removeviews.sql') FROM dual;
